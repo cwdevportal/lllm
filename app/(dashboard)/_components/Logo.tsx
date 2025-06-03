ec5e64bf9fbb34168502ff7ef796d2194969d256
Offor john <offorchukwuebuka11@gmial.com>
2025-06-03 06:03:45 -0700

    aa

--- app/(dashboard)/_components/Logo.tsx
+++ app/(dashboard)/_components/Logo.tsx
@@ -1,11 +1,13 @@
-import Image from 'next/image'
+'use client'
 
+import Image from 'next/image'
+import Link from 'next/link'
 
 export const Logo = () => {
   return (
-    <div className='flex items-center'>
-        <Image src="/logo.svg" height={30} width={30} alt='Logo' />
-        <span className='ml-4 text-[#0369a1] font-semibold'>LMS</span>
-    </div>
+    <Link href="/" className="flex items-center cursor-pointer">
+      <Image src="/logo.svg" height={30} width={30} alt="Logo" />
+      <span className="ml-4 text-[#0369a1] font-semibold">LMS</span>
+    </Link>
   )
 }
